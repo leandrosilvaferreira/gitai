edebe0e0e9a81216bee2046e11e330f6a0945209
leandrosilvaferreira <leandrosilvaferreira@gmail.com>
2024-11-12 21:32:17 -0400

    feat: update Gitai version and improve git command handling
    Updated Gitai version from 0.2.0-beta to 0.5.1-beta in gitai.py. Improved git command handling by adding error handling for 'git pull' and 'git commit' operations, including the use of a temporary file for commit messages. Added functionality to detect uncommitted changes and branch status, enhancing the robustness of git operations and reducing manual conflict resolution.

--- src/gitai/gitai.py
+++ src/gitai/gitai.py
@@ -3,11 +3,12 @@ import subprocess
 import sys
 from textwrap import dedent
 import argparse
+import tempfile
 
 from dotenv import load_dotenv
 
 exe_dir = os.path.dirname(sys.executable)
-print('Gitai v.0.2.0-beta')
+print('Gitai v.0.5.1-beta')
 print('exe_dir:', exe_dir)
 
 # Constrói o caminho do arquivo .env
@@ -66,7 +67,7 @@ def detect_project_language(project_path):
     return "Desconhecido"
 
 
-def generate_commit_message(status_output, project_language, base_message):
+def generate_commit_message(diff_output, project_language, base_message):
     prompt = dedent(f"""
     Com base nas informações fornecidas abaixo, crie uma mensagem de commit seguindo o padrão de Conventional Commits, 
     que é amplamente adotado para tornar as mensagens de commit mais descritivas e úteis. Este padrão utiliza prefixos 
@@ -81,14 +82,14 @@ def generate_commit_message(status_output, project_language, base_message):
 
     Descrição básica da mudança fornecida pelo programador a qual você deve usar como base para o início da sua mensagem: '{base_message}'
 
-    Abaixo seguem as mudanças detalhadas (incluindo arquivos modificados e o que foi incluído, alterado ou excluído) geradas pelo comando 'git status': 
+    Abaixo seguem as mudanças detalhadas (incluindo arquivos modificados e o que foi incluído, alterado ou excluído) geradas pelo comando 'git diff': 
 
     ```
-    {status_output}
+    {diff_output}
     ```
 
     Com base nas informações acima, melhore a descrição básica para criar uma mensagem de commit objetiva.
-    
+
     Regras obrigatórias:
     - Você deve seguir o padrão de Conventional Commits.
     - A primeira linha da mensagem deve obrigatoriamente começar com um dos prefixos do padrão de Conventional Commits (feat, fix, chore, etc.) seguido de uma descrição concisa que explique o que foi feito
@@ -112,7 +113,7 @@ def call_provider_api(prompt):
                             Você é um assistente que ajuda a gerar mensagens de commit para um repositório Git. 
                             As mensagens de commit devem seguir o padrão de Conventional Commits, que usa prefixos específicos para categorizar o tipo de mudança realizada (feat, fix, chore, etc.). 
                             A descrição deve ser concisa e clara, explicando o que foi feito, o motivo da mudança e, se aplicável, o impacto da mudança.
-                            As mensagens devem ser geradas com base nas alterações fornecidas pelo comando 'git status' e em uma descrição básica fornecida pelo usuário opcionalmente. 
+                            As mensagens devem ser geradas com base nas alterações fornecidas pelo comando 'git diff' e em uma descrição básica fornecida pelo usuário opcionalmente. 
 
                             Regras obrigatórias:
                             - NÃO adicione comentários ou explicações adicionais além da mensagem de commit gerada.
@@ -158,27 +159,58 @@ def call_provider_api(prompt):
             sys.exit(1)
 
 
-def run_git_command(command):
-    try:
-        result = subprocess.run(command, check=True, text=True, capture_output=True, encoding='utf-8')
-        return result.stdout.strip(), result.returncode
-    except subprocess.CalledProcessError as e:
-        print(f"Erro ao executar o comando:\n-> {command}\nerror: {e.stderr}")
-        print(f"\n\nSaída de erro padrão:\n {e.output}")
-        sys.exit(1)
+def run_git_command(command, exit_on_error=True):
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    if result.returncode != 0:
+        if exit_on_error:
+            print(f"Erro ao executar o comando:\n-> {' '.join(command)}")
+            print(f"Saída padrão:\n{result.stdout}")
+            print(f"Saída de erro:\n{result.stderr}")
+            sys.exit(1)
+        else:
+            output = result.stdout.strip() + '\n' + result.stderr.strip()
+            return output, result.returncode
+    else:
+        output = result.stdout.strip() + '\n' + result.stderr.strip()
+        return output, result.returncode
 
 
 def has_uncommitted_changes():
     status_output, _ = run_git_command(['git', 'status', '--porcelain'])
-    return len(status_output) > 0
+    return len(status_output.strip()) > 0
+
+
+def is_branch_ahead():
+    status_output, _ = run_git_command(['git', 'status', '-uno'])
+    return "Your branch is ahead" in status_output or "Seu branch está à frente" in status_output
 
 
 def commit_changes(commit_message):
-    while has_uncommitted_changes():
-        run_git_command(['git', 'add', '.'])
-        _, commit_result = run_git_command(['git', 'commit', '-m', commit_message])
-        if commit_result != 0:
-            break
+    run_git_command(['git', 'add', '.'])
+    # Escreve a mensagem de commit em um arquivo temporário
+    with tempfile.NamedTemporaryFile(mode='w', delete=False, encoding='utf-8') as temp_file:
+        temp_file.write(commit_message)
+        temp_filename = temp_file.name
+    try:
+        _, commit_result = run_git_command(['git', 'commit', '-F', temp_filename])
+    finally:
+        os.unlink(temp_filename)  # Remove o arquivo temporário
+
+
+def perform_git_pull():
+    output, returncode = run_git_command(['git', 'pull'], exit_on_error=False)
+
+    if returncode != 0:
+        if 'CONFLICT' in output or 'CONFLITO' in output:
+            print("Conflitos detectados durante git pull:")
+            print(output)
+            return False
+        else:
+            print(f"Erro ao executar git pull:\n{output}")
+            sys.exit(1)
+    else:
+        print("Git pull executado com sucesso.")
+        return True
 
 
 def main():
@@ -193,29 +225,51 @@ def main():
     args = parser.parse_args()
     os.chdir(args.project_path)
 
-    project_language = detect_project_language(args.project_path)
-    status_output, _ = run_git_command(['git', 'status'])
-    diff_output, _ = run_git_command(['git', 'diff'])
+    # Verifica se há mudanças não commitadas antes do git pull
+    if has_uncommitted_changes():
+        print("Alterações locais não commitadas detectadas.")
+        project_language = detect_project_language(args.project_path)
+        diff_output, _ = run_git_command(['git', 'diff'])
+        commit_message = generate_commit_message(diff_output, project_language, args.base_message)
 
-    commit_message = generate_commit_message(status_output + "\n" + diff_output, project_language, args.base_message)
+        print("\n\nMensagem de commit gerada:\n")
+        print(commit_message)
+        print("\n")
 
-    print("\n\nMensagem de commit gerada:\n")
-    print(commit_message)
-    print("\n")
+        commit_changes(commit_message)
+        print("-> Gitai concluiu o commit das alterações locais com sucesso.")
+    else:
+        print("Nenhuma mudança local a ser commitada antes do git pull.")
 
-    commit_changes(commit_message)
-    print("-> Gitai concluiu o commit com sucesso.")
+    # Executa git pull após commitar as alterações locais
+    pull_successful = perform_git_pull()
+
+    if not pull_successful:
+        print("Git pull falhou devido a conflitos. Por favor, resolva os conflitos manualmente.")
+        sys.exit(1)
+
+    # Verifica se houve novos conflitos após o pull
+    if has_uncommitted_changes():
+        print("Conflitos ou mudanças não commitadas após o pull detectadas.")
+        project_language = detect_project_language(args.project_path)
+        diff_output, _ = run_git_command(['git', 'diff'])
+        commit_message = generate_commit_message(diff_output, project_language, "Resolvendo conflitos após git pull")
+
+        print("\n\nMensagem de commit gerada para resolver conflitos:\n")
+        print(commit_message)
+        print("\n")
+
+        commit_changes(commit_message)
+        print("-> Gitai concluiu o commit das mudanças após o pull com sucesso.")
+    else:
+        print("Nenhuma mudança a ser commitada após o git pull.")
 
     if args.push:
-        run_git_command(['git', 'fetch'])
-        status_output, _ = run_git_command(['git', 'status', '-uno'])
-
-        if 'have diverged' in status_output:
-            print(f"Existem alterações remotas no branch atual, faça merge primeiro antes de fazer push.\nMsg:\n{status_output}")
-            sys.exit(1)
-
-        run_git_command(['git', 'push'])
-        print("-> Gitai concluiu o push com sucesso.")
+        if is_branch_ahead():
+            run_git_command(['git', 'push'])
+            print("-> Gitai concluiu o push com sucesso.")
+        else:
+            print("Nenhuma mudança para enviar. O branch local está sincronizado com o remoto.")
 
 
 if __name__ == "__main__":
